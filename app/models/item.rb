7c4a164abeef59000a3b8327cfe5cf0ded1bff70
Cathy Pham <dctpham@uwaterloo.ca>
2022-01-19 04:10:02 -0500

    Start editing views

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
+    has_and_belongs_to_many :tags
+
     validates_presence_of :name, :retail_price, :wholesale_price, :supplier
     validates_length_of :name, maximum: 70
-    
+    validates_length_of :supplier, maximum: 70
+    validates_uniqueness_of :name, case_sensitive: false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root 'static_pages#home'
   resources :tags
   resources :items
+  get 'items/:id/confirm_delete', to: 'items#confirm_delete', as: 'confirm_delete_item'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

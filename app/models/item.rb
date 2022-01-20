c9d0877166d6736d141f3eca48ec9bfa13bdb3d7
Cathy Pham <70992104+cathy-dctp@users.noreply.github.com>
2022-01-19 23:45:18 -0500

    Finalize project
    * Finish basic views of the inventory
    
    * Add varations, created_at, last_update columns
    
    * Add tags column and edit options
    
    * Progress on filtering

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,49 @@ class Item < ApplicationRecord
     validates_length_of :name, maximum: 70
     validates_length_of :supplier, maximum: 70
     validates_uniqueness_of :name, case_sensitive: false
+
+    filterrific(
+      # default_filter_params: { sorted_by: 'updated_at_desc' },
+      available_filters: [
+        # :sorted_by,
+        :search_query,
+        :with_tags,
+        # :with_quantity,
+        # :with_supplier,
+      ]
+    )
+
+    scope :with_tags, ->(tag_name) {
+      # tags_names = tag_names.split(",").map(&:strip)
+      Tag.where(name: tag_name).items
+    }
+
+    scope :search_query, ->(query) {
+      return nil  if query.blank?
+
+      # condition query, parse into individual keywords
+      terms = query.downcase.split(/\s+/)
+
+      # replace "*" with "%" for wildcard searches,
+      # append '%', remove duplicate '%'s
+      terms = terms.map { |e|
+        (e.tr("*", "%") + "%").gsub(/%+/, "%")
+      }
+      # configure number of OR conditions for provision
+      # of interpolation arguments. Adjust this if you
+      # change the number of OR conditions.
+      num_or_conds = 3
+      where(
+        terms.map { |_term|
+          or_clauses = [
+            "LOWER(items.name) LIKE ?",
+            "LOWER(items.desc) LIKE ?",
+            "LOWER(items.supplier) LIKE ?"
+          ].join(' OR ')
+          "(#{ or_clauses })"
+        }.join(' AND '),
+        *terms.map { |e| [e] * num_or_conds }.flatten
+      )
+    }
+
 end
